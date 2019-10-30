4216813e0727df163f98a2a3a884b95c5b9fcb80
Or Schneider <or@homeis.com>
2019-10-30 16:05:16 +0200

    FIX-FORWARD fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,35 @@
-import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
-import pkg from './package.json';
+import babel from "rollup-plugin-babel";
+import uglify from "rollup-plugin-uglify";
+import pkg from "./package.json";
 
 const mergeAll = objs => Object.assign({}, ...objs);
 
 const commonPlugins = [
   babel({
-    exclude: 'node_modules/**',
-  }),
+    exclude: "node_modules/**"
+  })
 ];
 
 const configBase = {
-  input: 'src/index.js',
-  external: Object.keys(pkg.dependencies || {}).concat(Object.keys(pkg.peerDependencies || {})),
-  plugins: commonPlugins,
+  input: "src/index.js",
+  external: Object.keys(pkg.dependencies || {}).concat(
+    Object.keys(pkg.peerDependencies || {})
+  ),
+  plugins: commonPlugins
 };
 
 const devUmdConfig = mergeAll([
   configBase,
   {
     output: {
-      file: pkg.unpkg.replace(/\.min\.js$/, '.js'),
-      format: 'umd',
-      name: 'onClickOutside',
-      exports: 'named',
+      file: pkg.unpkg.replace(/\.min\.js$/, ".js"),
+      format: "umd",
+      name: "onClickOutside",
+      exports: "named"
     },
-    globals: { react: 'React', 'react-dom': 'ReactDOM' },
-    external: Object.keys(pkg.peerDependencies || {}),
-  },
+    globals: { react: "React", "react-dom": "ReactDOM" },
+    external: Object.keys(pkg.peerDependencies || {})
+  }
 ]);
 
 const prodUmdConfig = mergeAll([
@@ -39,22 +41,21 @@ const prodUmdConfig = mergeAll([
         compress: {
           pure_getters: true,
           unsafe: true,
-          unsafe_comps: true,
-          warnings: false,
-        },
+          unsafe_comps: true
+        }
       })
-    ),
-  },
+    )
+  }
 ]);
 
 const webConfig = mergeAll([
   configBase,
   {
     output: [
-      { file: pkg.module, format: 'es' },
-      { file: pkg.main, format: 'cjs', exports: 'named' }
-    ],
-  },
+      { file: pkg.module, format: "es" },
+      { file: pkg.main, format: "cjs", exports: "named" }
+    ]
+  }
 ]);
 
 export default [devUmdConfig, prodUmdConfig, webConfig];
